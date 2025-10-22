d8ca05276d42e0b586f1ed634a95220eb128d6c4
tomproux <tomproux53@gmail.com>
2025-10-22 07:38:45 +0000

    ok

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	for (j = 65; j < 90; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+
+	return (0);
+}

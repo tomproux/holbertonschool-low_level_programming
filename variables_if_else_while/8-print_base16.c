421d9c0c670ce831f56be1e5fae6869711926545
tomproux <tomproux53@gmail.com>
2025-10-22 08:27:14 +0000

    ok

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j < 103; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+
+	return (0);
+}

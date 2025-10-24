430ee93f33f2ed180f10ac20c2edbdd7b10dc309
tomproux <77828137+tomproux@users.noreply.github.com>
2025-10-24 10:55:03 +0200

    Update 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+*print_alphabet_x10- ecrire l'alphabet en 10 fois
+*Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+
+	char c;
+	char e;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (e = 'a'; e <= 'z'; e++)
+		{
+		_putchar(e);
+		}
+	_putchar('\n');
+	}
+}

4ec82fa47ad2a4a2b749eb0fa9b66d0b3a9356a9
tomproux <77828137+tomproux@users.noreply.github.com>
2025-10-24 10:50:38 +0200

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+* print_alphabet- Starfoula je te alala betty
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}

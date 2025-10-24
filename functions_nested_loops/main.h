40103e1f892287f15b74305ce5f935e904cf671b
tomproux <77828137+tomproux@users.noreply.github.com>
2025-10-24 11:01:49 +0200

    Update main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char c);
+void print_alphabet(void);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+void print_alphabet_x10(void);
+
+#endif /* MAIN_H */

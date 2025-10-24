f89510388a0be65175166c76666df885334fb2d1
tomproux <tomproux53@gmail.com>
2025-10-24 12:48:41 +0000

    ok

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,14 @@
 
 int print_last_digit(int i)
 {
-	i = i % 10;
-
 	if (i > 0)
 	{
-		return (i);
+		return (i % 10);
 	}
 	else if (i < 0)
 	{
+		i = i % 10;
 		return (-i);
 	}
-	return (i);
+	return (i % 10);
 }

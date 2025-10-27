dcf0dc797bc389050ab26baa0cf683b7b835657d
tomproux <tomproux53@gmail.com>
2025-10-27 09:53:25 +0000

    ok

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,10 @@
+#include "main.h"
+
 int _isupper(int c)
 {
-	for (c = 32; c < 127; c++)
+	if (c > 64 && c < 91)
 	{
-		if (c < 65)
-		{
-			return (0);
-		}
-		else if (c > 90)
-		{
-			return (0);
-		}
-		else
-		{
-			return (1);
-		}
+		return (1);
 	}
 	return (0);
 }

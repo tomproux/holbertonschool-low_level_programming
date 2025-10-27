48377260788e90e7c6f580b9d7728756c5862425
tomproux <tomproux53@gmail.com>
2025-10-27 09:17:45 +0000

    ok

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,14 @@ int _isupper(int c)
 {
 	for (c = 32; c < 127; c++)
 	{
-		while (c < 65 && c > 90)
+		if (c < 65 && c > 90)
 		{
 			return (0);
 		}
-		return (1);
+		else
+		{
+			return (1);
+		}
 	}
 	return (-1);
 }

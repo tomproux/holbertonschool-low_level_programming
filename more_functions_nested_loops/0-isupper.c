b0f8f4c42ec4f849cb5ea4a5f8395e1a0801d80c
tomproux <tomproux53@gmail.com>
2025-10-27 09:59:33 +0000

    ok

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+* _isupper- elle ecris les signes coucou c'est Tom
+* @c: permet de donner la valeur absolu d'un int
+* Return: Always 0 (Success)
+*/
 int _isupper(int c)
 {
 	if (c > 64 && c < 91)

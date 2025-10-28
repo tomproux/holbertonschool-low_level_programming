7b37f3bbb98937254446a62d7727ae8fc715ee8d
tomproux <tomproux53@gmail.com>
2025-10-28 09:25:46 +0000

    ok

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main- elle affiche les chiffres de 1 a 100
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 1; n < 101; n++)
+	{
+		if (n == n % 3)
+		{
+			printf("Fizz");
+		}
+		else if (n == n % 5)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+
+	return (0);
+}

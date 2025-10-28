6c999bf9eb9a7017a3d000c286f5d3d59cdb3860
tomproux <tomproux53@gmail.com>
2025-10-28 11:43:01 +0000

    ok

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -13,9 +13,9 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = i; j > 0; j--)
+			for (j = size - i; j > 0; j--)
 			{
 				_putchar(' ');
 			}

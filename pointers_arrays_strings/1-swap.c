6a8e796385d61f73d1cea78ea714d211fb67e2be
tomproux <tomproux53@gmail.com>
2025-10-28 12:39:33 +0000

    ok

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* swap_int- permet de swap deux entiers
+* @a: premier pointeur entier
+* @b: deuxieme pointeur entier
+*
+*/
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = 0;
+	temp = *a;
+	*a = *b;
+	b = &temp;
+}

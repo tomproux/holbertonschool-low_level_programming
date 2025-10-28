7c4e561c4167396ea54a7eaba312ad3611ba5a06
tomproux <tomproux53@gmail.com>
2025-10-28 13:00:09 +0000

    ok

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen- retourne la longueur d'une chaine de caractere
+* @s: valeur parametre d'une chaine de caractere
+* Return: Always 0 (Success)
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	return (len);
+}

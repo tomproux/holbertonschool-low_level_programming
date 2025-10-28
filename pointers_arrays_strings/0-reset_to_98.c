fb309bb295f1bc47ba6f20f044ce96e78eaeff27
tomproux <tomproux53@gmail.com>
2025-10-28 12:30:41 +0000

    ok

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-/*
+/**
 * reset_to_98- initialise un pointeur ayant la valeur 98
-* @n: pointeur parametre
+* @n: pointeur parametre ayant pour valeur 98
 *
 */
 void reset_to_98(int *n)

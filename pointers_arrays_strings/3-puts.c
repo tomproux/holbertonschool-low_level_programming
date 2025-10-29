134c9140576fc7018361a72bc0c0a1ada8015418
tomproux <tomproux53@gmail.com>
2025-10-29 08:32:36 +0000

    ok

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts- affiche une chaine de caractere
+* @str: valeur parametre d'une chaine de caractere
+*
+*/
+void _puts(char *str)
+{
+	printf("%s/n", str);
+}

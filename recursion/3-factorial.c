ab04cfdfc91e7096d6671903dc0eac8825328fad
tomproux <tomproux53@gmail.com>
2025-11-04 15:12:42 +0100

    ok

---
+++ recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a number
+ * @n: number to return the factorial from
+ *
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+
+}

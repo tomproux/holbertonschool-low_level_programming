10350e44c97b90b6dfe12c00e9959bdbe64da62e
tomproux <tomproux53@gmail.com>
2025-11-10 14:25:11 +0000

    ok

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,15 +15,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	if (size == 0)
-    {
+	{
 		return (NULL);
-    }
+	}
 	p = (char *) malloc(sizeof(char) * size);
 
 	if (p == NULL)
-    {
+	{
 		return (NULL);
-    }
+	}
 	while (i < size)
 	{
 		*(p + i) = c;

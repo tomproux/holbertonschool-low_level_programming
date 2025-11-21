1724c3e5cf62b9e3e52bb687d8ddc64989ed3a7b
tomproux <tomproux53@gmail.com>
2025-11-21 11:28:18 +0000

    ok

---
+++ function_pointers/3-main.c
@@ -0,0 +1,19 @@
+#include "3-calc.h"
+
+/**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
+
+	
+
+	return (0);
+}
